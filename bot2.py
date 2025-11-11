47563e3821ae91699e674ceac9c58c01a4402489
Diovola <68344993+Diovola@users.noreply.github.com>
2025-11-12 04:12:50 +0800

    Update bot2.py
    4.2.1 debug 2

--- bot2.py
+++ bot2.py
@@ -41,12 +41,12 @@ async def on_voice_state_update(member, before, after):
 
     # 加入語音頻道
     if before.channel is None and after.channel is not None:
-        msg = f"> ## 🎧 {member.display_name} ''' 在 {current_time}加入了<#{after.channel.id}>"
+        msg = f"> ## 🎧 {member.display_name}\n> 在 {current_time}加入了<#{after.channel.id}>"
         await text_channel.send(msg)
 
     # 離開語音頻道
     elif before.channel is not None and after.channel is None:
-        msg = f"> ## 👋 {member.display_name} ''' 在 {current_time} 離開了語音頻道 <#{before.channel.id}>"
+        msg = f"> ## 👋 {member.display_name}\n> 在 {current_time} 離開了語音頻道 <#{before.channel.id}>"
         await text_channel.send(msg)
 
     # 在語音頻道之間移動
@@ -87,6 +87,7 @@ if not token:
     sys.exit(1)
 
 bot.run(token)
+
 
 
 
